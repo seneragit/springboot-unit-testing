4d94b9dfadb62680a514c4df2df1a4f0564614de
Senera Mallikarachchi <45624241+seneragit@users.noreply.github.com>
2023-05-21 13:55:06 +0530

    unit test findAll repository

--- src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -4,10 +4,13 @@ import com.pokemonreview.api.models.Pokemon;
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.Timeout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -31,5 +34,26 @@ public class PokemonRepositoryTests {
         //Assert
         Assertions.assertThat(savedPokemon).isNotNull();
         Assertions.assertThat(savedPokemon.getId()).isGreaterThan(0);
+    }
+    @Test
+    public void PokemonRepository_GetAll_ReturnsMoreThanOnePokemon() {
+
+        Pokemon pokemon1 = Pokemon.builder()
+                .name("kasun")
+                .type("electric").build();
+
+        Pokemon pokemon2 = Pokemon.builder()
+                .name("nisal")
+                .type("electric").build();
+
+        pokemonRepository.save(pokemon1);
+        pokemonRepository.save(pokemon2);
+
+        List<Pokemon> allPokemon = pokemonRepository.findAll();
+
+        Assertions.assertThat(allPokemon).isNotNull();
+        Assertions.assertThat(allPokemon.size()).isEqualTo(2);
+
+
     }
 }
