f4f4fbbb486ddd03efec5d496cf42e3ab31b00ae
Senera Mallikarachchi <45624241+seneragit@users.noreply.github.com>
2023-05-21 12:20:30 +0530

    unit test repository

---
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -0,0 +1,37 @@
+package com.pokemonreview.api.repository;
+
+import com.pokemonreview.api.models.Pokemon;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+public class PokemonRepositoryTests {
+
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    @Test
+    public void PokemonRepository_SaveAll_ReturnSavedPokemon() {
+
+        //Arrange
+
+        Pokemon pokemon = Pokemon.builder()
+                .name("senera")
+                .type("electric").build();
+
+        //Act
+        Pokemon savedPokemon = pokemonRepository.save(pokemon);
+
+        //Assert
+        Assertions.assertThat(savedPokemon).isNotNull();
+        Assertions.assertThat(savedPokemon.getId()).isGreaterThan(0);
+    }
+}
